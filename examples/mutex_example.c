e7458c5b65fc9bc1c9edd98126db682aa10f30b9
Nikita Fediuchin <cfnptr@gmail.com>
2021-03-12 17:20:20 +0200

    Add examples

---
+++ examples/mutex_example.c
@@ -0,0 +1,93 @@
+#include "mpmt/mutex.h"
+#include "mpmt/thread.h"
+
+#include <stdio.h>
+
+struct ThreadData
+{
+	struct Mutex* mutex;
+	int threadIndex;
+};
+
+static void threadFunction(void* argument)
+{
+	struct ThreadData* threadData =
+		(struct ThreadData*)argument;
+
+	lockMutex(threadData->mutex);
+
+	printf("Thread index: %d\n",
+		threadData->threadIndex);
+	fflush(stdout);
+
+	unlockMutex(threadData->mutex);
+}
+
+int main()
+{
+	struct Mutex* mutex = createMutex();
+
+	if (mutex == NULL)
+		return EXIT_FAILURE;
+
+	struct ThreadData* firstThreadData = malloc(
+		sizeof(struct ThreadData));
+
+	if (firstThreadData == NULL)
+	{
+		destroyMutex(mutex);
+		return EXIT_FAILURE;
+	}
+
+	firstThreadData->mutex = mutex;
+	firstThreadData->threadIndex = 1;
+
+	struct ThreadData* secondThreadData = malloc(
+		sizeof(struct ThreadData));
+
+	if (secondThreadData == NULL)
+	{
+		free(firstThreadData);
+		destroyMutex(mutex);
+		return EXIT_FAILURE;
+	}
+
+	secondThreadData->mutex = mutex;
+	secondThreadData->threadIndex = 2;
+
+	struct Thread* firstThread = createThread(
+		threadFunction,
+		firstThreadData);
+
+	if (firstThread == NULL)
+	{
+		free(secondThreadData);
+		free(firstThreadData);
+		destroyMutex(mutex);
+		return EXIT_FAILURE;
+	}
+
+	struct Thread* secondThread = createThread(
+		threadFunction,
+		secondThreadData);
+
+	if (secondThread == NULL)
+	{
+		destroyThread(firstThread);
+		free(secondThreadData);
+		free(firstThreadData);
+		destroyMutex(mutex);
+		return EXIT_FAILURE;
+	}
+
+	joinThread(secondThread);
+	destroyThread(secondThread);
+
+	joinThread(firstThread);
+	destroyThread(firstThread);
+
+	free(secondThreadData);
+	free(firstThreadData);
+	destroyMutex(mutex);
+	return EXIT_SUCCESS;
+}
---
+++ examples/thread_example.c
@@ -0,0 +1,36 @@
+#include "mpmt/thread.h"
+#include <stdio.h>
+
+static void threadFunction(void* argument)
+{
+	printf("Thread index: %s\n",
+		(const char*)argument);
+	fflush(stdout);
+}
+
+int main()
+{
+	struct Thread* firstThread = createThread(
+		threadFunction,
+		"1");
+
+	if (firstThread == NULL)
+		return EXIT_FAILURE;
+
+	struct Thread* secondThread = createThread(
+		threadFunction,
+		"2");
+
+	if (secondThread == NULL)
+	{
+		destroyThread(firstThread);
+		return EXIT_FAILURE;
+	}
+
+	joinThread(secondThread);
+	destroyThread(secondThread);
+
+	joinThread(firstThread);
+	destroyThread(firstThread);
+	return EXIT_SUCCESS;
+}
