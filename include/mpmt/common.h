5d054a43765bdf8f1e8cceee1bf1688068c9d53b
Nikita Fediuchin <cfnptr@gmail.com>
2022-02-18 14:08:02 +0200

    Add CPU name getter

--- include/mpmt/common.h
+++ include/mpmt/common.h
@@ -14,12 +14,15 @@
 
 #pragma once
 #include <stdlib.h>
+#include <string.h>
 
 #if __linux__ || __APPLE__
 #include <time.h>
 #include <unistd.h>
+#include <cpuid.h>
 #elif _WIN32
 #include <windows.h>
+#include <intrin.h>
 #else
 #error Unknown operating system
 #endif
@@ -75,4 +78,45 @@ inline static int getCpuCount()
 	GetSystemInfo(&systemInfo);
 	return (int)systemInfo.dwNumberOfProcessors;
 #endif
+}
+
+/*
+ * CPU brand string.
+ * Use getCpuName to get CPU name!
+ */
+static char cpuBrand[0x40];
+
+/*
+ * Returns running system CPU name string.
+ */
+inline static const char* getCpuName()
+{
+	unsigned int cpuInfo[4] = { 0, 0, 0, };
+
+#if __linux__ || __APPLE__
+	__cpuid(0x80000000, cpuInfo[0], cpuInfo[1], cpuInfo[2], cpuInfo[3]);
+#elif _WIN32
+	__cpuid(cpuInfo, 0x80000000);
+#endif
+
+	unsigned int nExIds = cpuInfo[0];
+	memset(cpuBrand, 0, sizeof(cpuBrand));
+
+	for (unsigned int i = 0x80000000; i <= nExIds; ++i)
+	{
+#if __linux__ || __APPLE__
+		__cpuid(i, cpuInfo[0], cpuInfo[1], cpuInfo[2], cpuInfo[3]);
+#elif _WIN32
+		__cpuid(cpuInfo, i);
+#endif
+
+		if (i == 0x80000002)
+			memcpy(cpuBrand, cpuInfo, sizeof(cpuInfo));
+		else if (i == 0x80000003)
+			memcpy(cpuBrand + 16, cpuInfo, sizeof(cpuInfo));
+		else if (i == 0x80000004)
+			memcpy(cpuBrand + 32, cpuInfo, sizeof(cpuInfo));
+	}
+
+	return cpuBrand;
 }
